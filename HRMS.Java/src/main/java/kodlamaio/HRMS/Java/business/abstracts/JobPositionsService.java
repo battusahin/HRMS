c6ca02cdcf0c53327f7344d6e95f449642d21dce
Batuhan Sahin <battusahin97@gmail.com>
2021-05-11 19:14:35 +0300

    first commit

---
+++ HRMS.Java/src/main/java/kodlamaio/HRMS/Java/business/abstracts/JobPositionsService.java
@@ -0,0 +1,9 @@
+package kodlamaio.HRMS.Java.business.abstracts;
+import java.util.List;
+import kodlamaio.HRMS.Java.entities.concretes.JobPositions;
+
+public interface JobPositionsService {
+    
+	List <JobPositions> getAll();
+
+}
